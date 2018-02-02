73698f69fd86d5d5bfaad46b8c7c6f9eb4ad62a4
Quentin Da Silva <32438087+Sh0ri@users.noreply.github.com>
2018-02-02 11:27:56 +0100

    node training

---
+++ Node/server.js
@@ -0,0 +1,21 @@
+let http = require('http');
+let fs = require('fs');
+
+let server = http.createServer();
+
+server.on('request',function(request,response){
+
+	fs.readFile('index.html', (err,data)=>{
+		if(err) throw err
+
+		response.writeHead(200,{
+		'Content-type': 'text/html; charset=utf-8'
+		})
+
+		response.end(data)
+	});
+
+
+})
+
+server.listen(8080)
