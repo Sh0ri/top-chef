35302de47b716d541f079595102842fd4a0e135b
Sh0ri <quent.dasilva@gmail.com>
2018-02-20 15:15:37 +0100

    a

---
+++ react_app/src/test_server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const server = require("./server.js");
+
+///////////////////////////////////////////////////////////////////EXPRESS///////////////////////////////////////////////////////////////////////////
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+  //server.store_offers();
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
