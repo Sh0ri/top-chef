233035cb95f778a27546194a1d2a8998db70b8d8
Quentin Da Silva <32438087+Sh0ri@users.noreply.github.com>
2018-03-04 13:42:36 +0100

    delete useless files

--- Node/my-app/src/scrape_michelin.js
+++
@@ -1,3 +0,0 @@
-const michelin = require("./michelin.js");
-
-michelin.scrape_michelin();
---
+++ install packages.cmd
@@ -0,0 +1,3 @@
+TITLE Install Packages
+start cmd /c install_package_server.cmd
+start cmd /c install_package_react.cmd
